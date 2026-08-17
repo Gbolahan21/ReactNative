262e466b325d8e94da7ae46488266aaee0b82de3
Sodiq-mohammed <mo@carrotcredit.com>
2026-08-17 12:48:23 +0100

    api

--- services/api.js
+++ services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://10.111.12.70:5000",
+  baseURL: "http://172.20.10.3:5000",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
