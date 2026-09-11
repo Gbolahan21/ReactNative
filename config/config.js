f5b99501b0d8930146367b002e3891209305191d
Sodiq-mohammed <mo@carrotcredit.com>
2026-09-11 16:38:32 +0100

    Ignore config folder

--- config/config.js
+++
@@ -1,9 +0,0 @@
-import Constants from 'expo-constants';
-
-const hostUri = Constants.expoConfig?.hostUri;
-
-const host = hostUri
-  ? hostUri.split(':')[0]
-  : 'localhost';
-
-export const BASE_URL = `http://${host}:5000`;
